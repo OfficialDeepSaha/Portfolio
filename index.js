5be8da4eec94ac17735c1cc02f0b1b9edee641e1
IT'S DEEP <deepsaha01896@gmail.com>
2024-01-16 01:00:26 +0530

    My Portfolio

---
+++ index.js
@@ -0,0 +1,11 @@
+var typed = new Typed(".typing", {
+
+    strings: [" ", "CEO Of DS-CART",
+        "Web Developer", "Frontend Developer", "Backend Developer", "FullStack Developer"
+    ],
+    typeSpeed: 100,
+    BackSpeed: 100,
+    loop: true
+
+
+})
